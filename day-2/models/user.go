1fb794aa72347bd1ea0b7f41d8c962211d01a737
IrvanAhmadP <irvan.ap1618@gmail.com>
2022-09-15 19:13:02 +0700

    added models

---
+++ day-2/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Username string `json:"username" form:"username"`
+}
